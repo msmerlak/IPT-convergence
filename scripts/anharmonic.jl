7f0e2d770570b4628e9bd624fecb80ff6d4f5f01
msmerlak <smerlak@mis.mpg.de>
2022-06-05 11:39:03 +0200

    preconditioned gradient descent?

--- scripts/anharmonic.jl
+++ scripts/anharmonic.jl
@@ -1,59 +1,104 @@
 using DrWatson, Plots
+using LinearAlgebra
 
 include(srcdir("hamiltonians.jl"))
-H = anharmonic_oscillator(10.; dim = 10_000)
+q = 8
+H = anharmonic_oscillator(1.; dim = 1000, order = q)
+X₀ = Matrix{eltype(H)}(I, size(H, 1), 1)
+
+plot(
+yaxis = :log, 
+xlabel = "iteration",
+ylabel = "residual norm",
+dpi = 500,
+title = " H = H₀ + X^$q"
+)
 
 ### standard solvers don't work
 using KrylovKit, Arpack, ArnoldiMethod
-using IterativeSolvers, Preconditioners
+
 KrylovKit.eigsolve(H, 1, :SR; verbosity = 0, maxiter = 5000)
 Arpack.eigs(H; nev = 1, which = :SR, maxiter = 5000)
 ArnoldiMethod.partialschur(H; nev = 1, which = SR())
-lobpcg(H, false, 1; maxiter = 5000)
 
-P = DiagonalPreconditioner(H)
-lobpcg(H, false, 1; P = P, maxiter = 5000)
 
-include(srcdir("davidson_herbst.jl"))
-davidson(H, H[:, 1:20])
+### LOBPCG with diagonal preconditioner
+
+## IterativeSolvers
+using IterativeSolvers, Preconditioners
+
+sol_lobpcg_iterativesolvers = lobpcg(H, false, X₀, 1; P = DiagonalPreconditioner(H), maxiter = 50_000, log = true, tol = 1e-10)
+plot!(
+    [t.residual_norms[1] for t in sol_lobpcg_iterativesolvers.trace[1]],
+    label = "LOBPCG with diag. precond."
+)
+
+## DFTK
+include(srcdir("lobpcg_levitt.jl"))
+D = Vector(diag(H))
+P = FunctionPreconditioner((y, x) -> y .= x./D)
+sol_lobpcg_dftk = LOBPCG(H, X₀, I, P, 1e-10, 1000)
+plot!(
+    vec(sol_lobpcg_dftk.residual_history),
+    label = "LOBPCG (DFTK) with diag. precond."
+)
+
+### PRIMME
+using PyCall, LinearAlgebra
+
+@pyimport primme
+@pyimport scipy.sparse as sp
+
+vals, vecs, info = primme.eigsh(
+    sp.csc_matrix(H), 1; 
+    v0 = X₀,
+    method=:PRIMME_JDQMR, 
+    which=:SA, 
+    tol = 1e-10,
+    OPinv=sp.spdiags(one(eltype(H)) ./ diag(H), [0], size(H)...), return_stats=true, return_history=true, maxiter=1e3,
+    convtest = (eval, evec, resNorm) -> resNorm < 1e-10
+    )
+
+plot!(
+    info["hist"]["resNorm"],
+    label = "PRIMME_DYNAMIC"
+)
+
 
 #### IPT
 
 using IterativePerturbationTheory
-include(srcdir("relaxed_ipt.jl"))
+include(srcdir("relaxed_IPT.jl"))
 
-plot(
-    yaxis = :log, 
-    xlabel = "iteration",
-    ylabel = "residual norm",
-    dpi = 500,
-    title = " H = H₀ + 10 X⁴"
-    )
-
-sol_lobpcg = lobpcg(H, false, 1; P = P, maxiter = 1000, log = true)
-plot!(
-    [t.residual_norms[1] for t in sol_lobpcg.trace],
-    label = "LOBPCG with diag. precond."
-)
 
 plot!(
-    relaxed_ipt(H, 1; α = .5, trace = true, tol = 6e-5, maxiters = 1000).errors,
-        label = "Simple relaxation (α = 1/2)"
+    relaxed_ipt(H, 1; α = .5, trace = true, tol = 1e-5, maxiters = 1000).errors,
+        label = "IPT - Simple relaxation (α = 1/2)"
     )
 
 plot!(
-    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 1, maxiters = 1000).trace,
-    label = "Anderson (m = 1)"
+    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 1, maxiter = 1000).trace,
+    label = "IPT - Anderson (m = 1)"
 )
 
 plot!(
-    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 5, maxiters = 1000).trace,
-    label = "Anderson (m = 5)"
+    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 5, maxiter = 50_000).trace,
+    label = "IPT - Anderson (m = 5)"
 )
 
 plot!(
-    ipt(H, 1; acceleration = :acx, trace = true, tol = 1e-13, maxiters = 1000).trace,
-    label = "ACX"
+    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 10, maxiter = 50_000).trace,
+    label = "IPT - Anderson (m = 10)"
+)
+
+plot!(
+    ipt(H, 1; acceleration = :anderson, trace = true, tol = 1e-10, anderson_memory = 50, maxiter = 50_000).trace,
+    label = "IPT - Anderson (m = 50)"
+)
+
+plot!(
+    ipt(H, 1; acceleration = :acx, trace = true, tol = 1e-10, maxiter = 50_000).trace,
+    label = "IPT - ACX"
 )
 savefig(plotsdir("anharmonic_traces"))
 
---
+++ scripts/gradient-descent.jl
@@ -0,0 +1,38 @@
+using DrWatson
+@quickactivate
+
+using DifferentialEquations
+using LinearAlgebra
+
+using Plots; gr(dpi = 500)
+
+ρ(H, x) = dot(x, H*x)/dot(x, x)
+
+function GD!(Y, X, p, t) 
+    H = p[:H] 
+    mul!(Y, H, X, -1, 1)
+    axpy!(-dot(X, Y)/dot(X, X), X, Y)
+    Y ./= diag(H)
+end
+
+include(srcdir("hamiltonians.jl"))
+
+H = anharmonic_oscillator(1., dim = 50, order = 6) 
+p = Dict(:H => H)
+X₀ = Matrix{Float64}(I, size(H, 1), 1)
+
+pb = ODEProblem(
+    GD!, X₀, (0, 10000.), 
+    Dict(:H => H);
+    callback = TerminateSteadyState()
+)
+
+sol = solve(pb)
+
+E₀ = minimum(eigen(Matrix(H)).values)
+
+plot(
+    sol.t, 
+    [ρ(H, x) - E₀ for x in sol.u];
+    yaxis = :log
+)
